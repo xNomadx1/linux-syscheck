1eaf5c118964d1ba8c91f947d585b8779beb98e7
xNomadx1 <tearnhardt71@outlook.com>
2026-05-02 14:36:43 -0700

    Feature: Add disk usage & main guard

--- syscheck.py
+++ syscheck.py
@@ -2,10 +2,7 @@
 
 import socket
 import subprocess
-
-from pathlib import Path
-
-AUTH_LOG = Path("/var/log/auth.log")
+import shutil
 
 def print_hostname():
     """Print the system hostname."""
@@ -15,3 +12,25 @@ def print_uptime():
     """Print system uptime using the uptime command."""
     print("\nUptime:")
     subprocess.run(["uptime"], check=False)
+
+def print_disk_usage():
+    """Print disk usage for the root filesystem."""
+    print("\nDisk usage")
+
+    usage =shutil.disk_usage("/")
+    total = usage.total / (1024 ** 3)
+    used = usage.total / (1024 ** 3)
+    free = usage.free / (1024 ** 3)
+
+    print(f"Total: {total:.2f} GB")
+    print(f"Used: {used: .2f} GB")
+    print(f"Free: {free: .2f} GB")
+
+def main():
+    """Run all system checks."""
+    print_hostname()
+    print_uptime()
+    print_disk_usage()
+
+if __name__ == "__main__":
+    main()
